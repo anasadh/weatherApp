ae292d5bf73ae2f5e37a191ff0a120dcea3163bf
Anamika Sadh <97339793+anasadh@users.noreply.github.com>
2023-06-30 02:37:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,60 +1,52 @@
 const http = require("http");
 const fs = require("fs");
-var requests = require("requests");
+const requests = require("requests");
 
-const homeFile = fs.readFileSync("home.html" , "utf-8");
-
-// const replaceVal = (tempVal, orgVal) => {
-//     let temperature =  tempVal.replace("{%tempVal%}", orgVal.main.temp);
-//      temperature =  temperature.replace("{%tempmin%}", orgVal.main.temp_min);
-//      temperature =  temperature.replace("{%tempmax%}", orgVal.main.temp_max);
-//      temperature =  temperature.replace("{%location%}", orgVal.name);
-//      temperature =  temperature.replace("{%country%}", orgVal.sys.country);
-//      temperature =  temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
-
-//      return temperature;
-// }
+const homeFile = fs.readFileSync("index.html", "utf-8");
+const cssFile = fs.readFileSync("style.css", "utf-8");
 
 const replaceVal = (tempVal, orgVal) => {
-    return tempVal.replace("{%tempval%}", orgVal.main.temp)
-    .replace("{%tempmin%}", orgVal.main.temp_min)
-    .replace("{%tempmax%}", orgVal.main.temp_max)
+  const celsiusTemp = Math.round(orgVal.main.temp - 273.15);
+  const celsiusTempMin = Math.round(orgVal.main.temp_min - 273.15);
+  const celsiusTempMax = Math.round(orgVal.main.temp_max - 273.15);
+
+  return tempVal
     .replace("{%location%}", orgVal.name)
     .replace("{%country%}", orgVal.sys.country)
-    .replace("{%tempstatus%}", orgVal.weather[0].main)
+    .replace("{%tempval%}", celsiusTemp)
+    .replace("{%tempmin%}", celsiusTempMin)
+    .replace("{%tempmax%}", celsiusTempMax)
+    .replace("{%tempstatus%}", orgVal.weather[0].main);
 };
 
-const server = http.createServer((req,res) => {
-    if(req.url == "/"){
-        requests("https://api.openweathermap.org/data/2.5/weather?q=Sanchi&appid=9e82e4710bed6b7a5823ba2a7a95b43d")
-.on('data', (chunk) => {
-    const objdata = JSON.parse(chunk);
-    const arrData = [objdata];
-  console.log(arrData[0].main.temp);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    requests(
+      "https://api.openweathermap.org/data/2.5/weather?q=Sanchi&appid=9e82e4710bed6b7a5823ba2a7a95b43d"
+    )
+      .on("data", (chunk) => {
+        const objdata = JSON.parse(chunk);
+        const realTimeData = replaceVal(homeFile, objdata);
 
-  const kelvin = arrData[0].main.temp;
-  const celsius = kelvin - 273.15;
-  console.log(celsius);
-
-//   const realTimeData = arrData.map((val) => {
-//     // console.log(val.main);
-//     return replaceVal(homeFile, val);
-//   })
-  
-
-const realTimeData = arrData.map((val) => replaceVal(homeFile, val)).join("");
-res.write(realTimeData);
-
-console.log(realTimeData);
-})
-.on("end", (err) => {
-  if (err) return console.log('connection closed due to errors', err);
-  res.end();
-    });
-    }
-    else {
-        res.end("File not found");
-      }
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(realTimeData);
+        res.end();
+      })
+      .on("error", (err) => {
+        console.error("Error:", err.message);
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+      });
+  } else if (req.url === "/style.css") {
+    res.writeHead(200, { "Content-Type": "text/css" });
+    res.write(cssFile);
+    res.end();
+  } else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("File not found");
+  }
 });
 
-server.listen(2000, "127.0.0.1");
+server.listen(2000, "127.0.0.1", () => {
+  console.log("Server is running on http://127.0.0.1:2000/");
+});
